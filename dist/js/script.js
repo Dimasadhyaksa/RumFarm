3789785dc274d4d978e25f77f361fd1b002606a5
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-08-25 00:45:50 +0700

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,22 +33,22 @@ window.onscroll = function () {
   });
   
   // Darkmode toggle
-  const darkToggle = document.querySelector('#dark-toggle');
-  const html = document.querySelector('html');
+  // const darkToggle = document.querySelector('#dark-toggle');
+  // const html = document.querySelector('html');
   
-  darkToggle.addEventListener('click', function () {
-    if (darkToggle.checked) {
-      html.classList.add('dark');
-      localStorage.theme = 'dark';
-    } else {
-      html.classList.remove('dark');
-      localStorage.theme = 'light';
-    }
-  });
+  // darkToggle.addEventListener('click', function () {
+  //   if (darkToggle.checked) {
+  //     html.classList.add('dark');
+  //     localStorage.theme = 'dark';
+  //   } else {
+  //     html.classList.remove('dark');
+  //     localStorage.theme = 'light';
+  //   }
+  // });
   
-  // pindahkan posisi toggle sesuai mode
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true;
-  } else {
-    darkToggle.checked = false;
-  }
+  // // pindahkan posisi toggle sesuai mode
+  // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  //   darkToggle.checked = true;
+  // } else {
+  //   darkToggle.checked = false;
+  // }
